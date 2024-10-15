fb0f719d1bddd210a37b005948cd141dba57b1ff
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-10-15 19:45:21 +0600

    Something added

---
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+
+
+function AppliedJobs() {
+    return (
+        <div>
+            <h1>Job is here</h1>
+        </div>
+    )
+}
+
+export default AppliedJobs
---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,11 @@
+
+
+    function Error() {
+    return (
+        <div>
+            <h2>Oops!!!</h2>
+        </div>
+    )
+}
+
+export default Error
