5559ef95b8ba2b4a21b8da6e620c4cace05d1950
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-10-17 20:06:06 +0600

    Some Changes Added

---
+++ src/components/JobCategory/JobCategory.jsx
@@ -0,0 +1,15 @@
+
+
+const JobCategory = () => {
+    return (
+        <div className="max-w-[90%] xl:max-w-6xl">
+            
+            <div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default JobCategory
