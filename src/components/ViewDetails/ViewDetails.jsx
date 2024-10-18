966169f392bf1fc5129dca8a0e78dada5f152441
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-10-18 10:57:09 +0600

    Button done

--- src/components/ViewDetails/ViewDetails.jsx
+++ src/components/ViewDetails/ViewDetails.jsx
@@ -1,6 +1,6 @@
 import { CiLocationOn } from "react-icons/ci";
 import { CiDollar } from "react-icons/ci";
-import { useLoaderData, useParams } from 'react-router-dom'
+import { Link, useLoaderData, useParams } from 'react-router-dom'
 
 const ViewDetails = () => {
 
@@ -12,7 +12,7 @@ const ViewDetails = () => {
     return (
         <section className=" pt-16 pb-20">
             <div className="flex flex-col justify-center items-center">
-                <div className=" w-[300px] p-4 border border-gray-400 rounded-lg shadow-xl text-gray-500">
+                <div className=" w-[400px] p-4 border border-gray-400 rounded-lg shadow-xl text-gray-500">
                     <figure>
                         <img
                         className='h-[90px] w-full'
@@ -40,6 +40,14 @@ const ViewDetails = () => {
                     <p className="mt-4 text-black font-semibold">Email : {email}</p>
                     <p className="mt-3 mb-3 text-black font-semibold">Phone : {phone}</p>
                     <p className="text-black font-semibold">Address : {address}</p>
+
+                    <button className="mt-5">
+                        <Link to={'/'} href="#_" className="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-primary rounded-lg group">
+                            <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+                            <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+                            <span className="relative">Back Home</span>
+                        </Link>
+                    </button>
                 </div>
             </div>
         </section>
